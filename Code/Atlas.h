e94aa9f7226a56d6560469e3d989109a7b11666d
Parth Kshatriya <parth0072@live.com>
2017-11-11 12:30:53 +0530

    - init

--- Code/Atlas.h
+++ Code/Atlas.h
@@ -91,13 +91,6 @@
 #import "ATLProgressView.h"
 #import "ATLUIImageHelper.h"
 
-///-----------------
-/// @name Categories
-///-----------------
-
-#import "LYRIdentity+ATLParticipant.h"
-#import "UIResponder+ATLFirstResponder.h"
-
 /**
  @abstract Returns the Atlas version as a string.
  */
